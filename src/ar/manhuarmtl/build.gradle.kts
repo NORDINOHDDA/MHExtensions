8c20dbf4e8fef468ae1817eff00c173d42fb195a
NORDINOHDDA <100313816+NORDINOHDDA@users.noreply.github.com>
2026-09-03 04:14:28 +0100

    fix: create build.gradle.kts for ar/manhuarmtl extension

---
+++ src/ar/manhuarmtl/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    alias(kei.plugins.extension)
+}
+
+keiyoushi {
+    name = "ManhuaRMTL"
+    versionCode = 2
+    theme = "madara"
+    isNsfw = false
+
+    source {
+        lang = "ar"
+        baseUrl = "https://manhuarmtl.com"
+    }
+}
