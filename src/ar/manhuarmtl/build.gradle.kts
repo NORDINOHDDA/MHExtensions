95da4142025dacb95971fca9022668613cd3ceff
NORDINOHDDA <100313816+NORDINOHDDA@users.noreply.github.com>
2026-09-03 03:21:17 +0100

    feat: add build.gradle.kts for ManhuaRMTL extension

---
+++ src/ar/manhuarmtl/build.gradle.kts
@@ -0,0 +1,10 @@
+dependencies {
+    implementation(project(":core"))
+}
+
+ext {
+    set("id", "ar.manhuarmtl")
+    set("version", "1.0.0")
+    set("name", "ManhuaRMTL (Arabic Translation)")
+    set("baseUrl", "https://manhuarmtl.com")
+}
